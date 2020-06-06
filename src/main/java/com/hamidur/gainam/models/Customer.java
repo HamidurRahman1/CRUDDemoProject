00da759448158406fb8f9fcb1fcf99fc0de660a6
Hamidur Rahman <Hamidur0405@gmail.com>
2020-06-05 21:54:02 -0400

    Application class added, model @ed, property configured, demo data added

--- src/main/java/com/hamidur/gainam/models/Customer.java
+++ src/main/java/com/hamidur/gainam/models/Customer.java
@@ -1,17 +1,49 @@
 package com.hamidur.gainam.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
 @Table(name = "customers")
 public class Customer
 {
+    @Id
+    @Column(name = "customer_id", updatable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer customerId;
+    @Column(name = "first_name", updatable = true, unique = false)
+    @NotNull(message = "first name cannot be null")
+    @NotBlank(message = "first name has too many white spaces")
+    @Size(min = 2, max = 30, message = "first name must be in length of 2 to 30 characters")
+    @Pattern(regexp = "^(?![\\s.]+$)[a-zA-Z\\s.]*$", message = "first name contains unexpected characters")
     private String firstName;
+
+
+    @Column(name = "last_name", updatable = true, nullable = true, unique = false)
+    @Size(min = 2, max = 30, message = "last name must be in length of 2 to 30 characters")
+    @Pattern(regexp = "^(?![\\s.]+$)[a-zA-Z\\s.]*$", message = "last name contains unexpected characters")
     private String lastName;
+
+    @Column(name = "email", updatable = true, nullable = false, unique = true)
+    @NotNull(message = "email cannot be null for a customer")
+    @Size(min = 3, max = 80, message = "email is too long to store")
+    @Pattern(regexp = "^[A-Za-z0-9_.-]+@[A-Za-z0-9_.-]+$",
+            message = "email and domain must only contain [letters, digits, dash, underscore, dot] only")
     private String email;
+
+    @Column(name = "country")
+    @NotNull(message = "country cannot be null")
+    @NotBlank(message = "country has too many white spaces")
+    @Size(min = 2, max = 25, message = "Country name must be in 2-25 characters")
     private String country;
 
     public Customer() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+insert into customers (first_name, last_name, email, country) values
+('first1 name', 'last1 name', 'my1.custom_email75@company.com', 'US'),
+('first2 name2', 'last2 name2', 'my2.custom_email75@company.com', 'Bangladesh'),
+('first3 name', 'last3 name', 'my3.custom_email75@company.com', 'India');
