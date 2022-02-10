7e9a65e3b7dc04aff7d4bb805e8e649e162edf7d
Hamidur Rahman <Hamidur0405@gmail.com>
2022-02-09 20:51:54 -0500

    adding test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,8 @@ insert into users (username, password, enabled) values
 ('admin', '$2b$10$jEDw4HHKDe75.mM47p4siui1XZrdGkf7Y0.oqKLY.Wt5TjIwuhByq', true),
 ('user', '$2b$10$UmpwL1ImYyVTy1eofo8t9.ecIFzyyCJsML8y0k1UJtMWG2W1hJ3T6', true);
 
+-- some comments
+
 -- ROLE_ prefix will be added at runtime
 insert into roles (role) values
 ('ADMIN'),
