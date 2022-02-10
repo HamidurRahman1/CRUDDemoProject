2cfd7d3462a65da861e13dc522e88d2ed9fe12bf
Hamidur Rahman <Hamidur0405@gmail.com>
2022-02-09 20:58:34 -0500

    adding test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 
 insert into users (username, password, enabled) values
-('admin', '$2b$10$jEDw4HHKDe75.mM47p4siui1XZrdGkf7Y0.oqKLY.Wt5TjIwuhByq', true, true),
+('admin', '$2b$10$jEDw4HHKDe75.mM47p4siui1XZrdGkf7Y0.oqKLY.Wt5TjIwuhByq', true),
 ('user', '$2b$10$UmpwL1ImYyVTy1eofo8t9.ecIFzyyCJsML8y0k1UJtMWG2W1hJ3T6', true);
 
 -- some comments
